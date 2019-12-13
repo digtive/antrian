655182572c2799d540da1e0fb352a4cd7d0cdd0c
azharsiddiq36 <azharsiddiq36@gmail.com>
2019-12-14 06:19:22 +0700

    Build Api Structure

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,15 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-	/*-- app routes --*/
-	$route['settings'] = 'AppController/settings';
-	$route['settings/colours'] = 'AppController/colours';
-	$route['settings/texts'] = 'AppController/texts';
-	$route['settings/audio'] = 'AppController/audio';
-	$route['settings/media'] = 'AppController/media';
-	$route['settings/print'] = 'AppController/prints';
-
-	$route['default_controller'] = 'AppController';
-	$route['404_override'] = '';
-	$route['translate_uri_dashes'] = FALSE;
+/*-- app routes --*/
+$route['settings'] = 'AppController/settings';
+$route['settings/colours'] = 'AppController/colours';
+$route['settings/texts'] = 'AppController/texts';
+$route['settings/audio'] = 'AppController/audio';
+$route['settings/media'] = 'AppController/media';
+$route['settings/print'] = 'AppController/prints';
+$route['api/queue/antrian'] = 'ApiController/antrian';
+$route['api/queue/call'] = 'ApiController/call';
+$route['api/queue/recall'] = 'ApiController/recall';
+$route['api/queue/layanan'] = 'ApiController/layanan';
+$route['default_controller'] = 'AppController';
+$route['mainpage'] = 'ResolutionController';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
 
