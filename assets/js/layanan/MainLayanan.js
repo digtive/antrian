a3c1fce857d3535a1458932094a60cbb0ecf689e
bagussjm <bagusibag85@gmail.com>
2020-03-26 08:00:07 +0700

    import library recta
    recta js. pos printer library

--- assets/js/layanan/MainLayanan.js
+++ assets/js/layanan/MainLayanan.js
@@ -9,24 +9,44 @@ $(document).ready(function(){
 		let url = $(this).data('url');
 		let response = services.getData('Services/'+url);
 		if (response.status === '200'){
-			$('#ticket-number-print').html(response.antrian_nomor);
-			$('#service-name-print').html(response.service_name);
-			$('#left-queue-print').html(response.left_queue);
-			swal({
-				title: 'BERHASIL MENGAMBIL ANTRIAN',
-				html: '<h4>Nomor Antrian Anda </h4><br>' +
-					'<h1>'+response.antrian_nomor+'</h1><br>' +
-					'<h5>Silahkan Cetak Karcis dan Menunggu</h5>'
-				,
-				type: 'success',
-				allowOutsideClick: false,
-				showCloseButton: false,
-				showCancelButton: false,
-				confirmButtonText:
-					'<button class="btn btn-success" id="cetak-karcis" data-antrian="'+response.antrian_nomor+'"><i class="fa fa-print"></i> cetak</button>',
-			});
+			receiptsPrint(response);
+			// $('#ticket-number-print').html(response.antrian_nomor);
+			// $('#service-name-print').html(response.service_name);
+			// $('#left-queue-print').html(response.left_queue);
+			// swal({
+			// 	title: 'BERHASIL MENGAMBIL ANTRIAN',
+			// 	html: '<h4>Nomor Antrian Anda </h4><br>' +
+			// 		'<h1>'+response.antrian_nomor+'</h1><br>' +
+			// 		'<h5>Silahkan Cetak Karcis dan Menunggu</h5>'
+			// 	,
+			// 	type: 'success',
+			// 	allowOutsideClick: false,
+			// 	showCloseButton: false,
+			// 	showCancelButton: false,
+			// 	confirmButtonText:
+			// 		'<button class="btn btn-success" id="cetak-karcis" data-antrian="'+response.antrian_nomor+'"><i class="fa fa-print"></i> cetak</button>',
+			// });
 		}
 	});
+
+	function receiptsPrint(response){
+		console.log(response);
+
+		let printer = new Recta('2706425049','1811');
+
+		printer.open().then(function () {
+			printer.align('center')
+				.text('Hello World !!')
+				.bold(true)
+				.text('This is bold text')
+				.bold(false)
+				.underline(true)
+				.text('This is underline text')
+				.underline(false)
+				.cut()
+				.print()
+		})
+	}
 
 	$(document).on('click','#cetak-karcis', function () {
 		// $('#ticket-preview').printThis({
